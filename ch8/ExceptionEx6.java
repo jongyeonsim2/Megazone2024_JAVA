17b6bc4da04fadec7ca8ba3922902ae35f104022
unknown <jongyeonsim2@gmail.com>
2024-05-30 11:19:22 +0900

    5th commit

--- ch8/ExceptionEx6.java
+++ ch8/ExceptionEx6.java
@@ -11,6 +11,14 @@ package ch8;
  * 	 사용 : AutoCloseable interface 를 구현.
  * 			인터페이스를 구현해야만, 자동 자원 반환 문법을 적용시킬 수 있음.
  * 
+ *         try{} block 에서도 예외가 발생하고, AutoCloseable 를 구현한
+ *         close() 에서도 예외가 발생한다면 ???
+ *         
+ *         두 군데서 발생할 예외적인 상황을 코드로 만들어야 확인이 가능함.
+ *         => 사용자 정의 Exception 을 두 개 만든 후,
+ *            두 개의 Exception 을 throw 시키면 됨.
+ *         
+ *         
  */
 
 
@@ -20,11 +28,14 @@ public class ExceptionEx6 {
 		// TODO Auto-generated method stub
 
 		try (CloseableResource cr = new CloseableResource()) {
-			
+			cr.resourceWork(false);
+		} catch(WorkException e) {
+			e.printStackTrace();
 		} catch(CloseException e) {
 			e.printStackTrace();
 		}
 		
+		System.out.println("정상 종료됨.");
 	}
 
 }
@@ -33,6 +44,14 @@ public class ExceptionEx6 {
 // 해제할 어떤 자원이 있다고 가정하고 class 를 작성.
 class CloseableResource implements AutoCloseable {
 
+	// 자원을 사용한다고 가정한 메소드
+	public void resourceWork(boolean exceptionFlag) throws WorkException {
+		System.out.println("resourceWork() 호출됨.");
+		
+		if(exceptionFlag)
+			// 발생한 예외는 위임처리 => 메소드 정의부분에 thorws 하면 됨.
+			throw new WorkException("WorkException 발생!!");
+	}
 	
 	/*
 	 * 사용자 정의 Exception 적용 전.
@@ -51,7 +70,14 @@ class CloseableResource implements AutoCloseable {
 	
 }
 
+// 자원을 사용시 발생하는 예외 처리용 Exception class
+class WorkException extends Exception {
+	public WorkException(String msg) {
+		super(msg);
+	}
+}
 
+// 자원 해제시 발생하는 예외 처리용 Exception class
 class CloseException extends Exception {
 	public CloseException(String msg) {
 		super(msg);
